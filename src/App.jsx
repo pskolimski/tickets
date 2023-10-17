23d4c2a5dd356a22938c65f91e765405f8c81052
Revku <revku@revku.dev>
2023-10-17 17:55:10 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TemplateInfo from 'TemplateInfo/TemplateInfo';
+
+const App = () => {
+   return (
+      <>
+         <TemplateInfo />
+      </>
+   );
+};
+
+export default App;
